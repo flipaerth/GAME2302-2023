2ea16e3c847eba55ae9392d3e495ff0f42d11b20
flipaerth <austinozan@gmail.com>
2023-09-19 23:33:45 -0500

    updated
    shows a version that does work

--- WeekThreeAssignment/WeekThreeAssignment.cpp
+++ WeekThreeAssignment/WeekThreeAssignment.cpp
@@ -187,3 +187,133 @@ int main()
 	
 	return(0);
 }
+
+
+// This version works, but I don't feel right submitting it, as it's not 100% my doing.
+
+/*
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Class of Student
+class Student
+{
+private:
+	string firstName;
+	string lastName;
+	int grade;
+
+public:
+
+	// Constructor
+	Student(string f, string l, int g) : firstName(f), lastName(l), grade(g) {}
+
+	// Getter for first name
+	string GetFirst() {
+		return firstName;
+	}
+
+	// Getter for last name
+	string GetLast() {
+		return lastName;
+	}
+
+	// Getter for grade
+	int GetGrade() {
+		return grade;
+	}
+
+};
+
+// Custom comparison function for sorting students by grade from highest to lowest
+bool CompareStudentsByGrade(Student& a, Student& b) {
+	return a.GetGrade() > b.GetGrade();
+}
+
+int main()
+{
+	// Create a vector of students (a list or an array of students)
+	vector<Student> students;
+
+	// Opens and reads the file
+	ifstream studentsFile("example.txt");
+
+	// The current ine of each string in the file
+	string currentLine;
+
+	// Variables
+	int numStudents = 0;
+	int average = 0;
+	int total = 0;
+
+	// If the file is open
+	if (studentsFile.is_open())
+	{
+
+		// Step through each line at least once while the file is open
+		while (getline(studentsFile, currentLine))
+		{
+			// Variables for student data
+			string firstName;
+			string lastName;
+			int grade;
+
+			// Finds the first space
+			size_t firstSpace = currentLine.find(" ");
+			// Sets the first name
+			firstName = currentLine.substr(0, firstSpace);
+			// Removes the first name after setting it
+			currentLine.erase(0, firstSpace + 1);
+
+			// Finds the second space
+			size_t secondSpace = currentLine.find(" ");
+			// Sets the last name
+			lastName = currentLine.substr(0, secondSpace);
+			// Removes the last name after setting it
+			currentLine.erase(0, secondSpace + 1);
+
+			// Converts the remainder of the string to an int and sets to grade
+			grade = stoi(currentLine);
+
+			// Create a Student object and add it to the vector
+			students.emplace_back(firstName, lastName, grade);
+
+			// Increment the total and number of students
+			total += grade;
+			numStudents++;
+		};
+
+		// Closes the file
+		studentsFile.close();
+	}
+	// Validation if the file cannot be opened
+	else
+	{
+		cout << "Unable to open the student's files." << endl;
+	}
+
+	// Sort the vector of students by grade in descending order using bubble sort
+	sort(students.begin(), students.end(), CompareStudentsByGrade);
+
+	// Output each student's information
+	for (Student& student : students)
+	{
+		cout << student.GetFirst() << " " << student.GetLast() << " " << student.GetGrade() << endl;
+	}
+
+	// Average Formula
+	average = total / numStudents;
+
+	// Display Average Output
+	cout << endl;
+	cout << "Numer of Students: " << numStudents << endl;
+	cout << "Grade Average: " << average << endl;
+
+	return(0);
+}
+*/
